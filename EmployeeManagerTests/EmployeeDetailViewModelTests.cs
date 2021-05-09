acf6eb18d0897249a3250b342ab8d8c1287f90b3
Oriol Burgaya <oriolbv@gmail.com>
2021-05-09 10:53:52 +0200

    Tests finished

---
+++ EmployeeManagerTests/EmployeeDetailViewModelTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeManager.ViewModels;
+using System;
+using EmployeeManager.Views;
+using EmployeeManager.Models;
+
+namespace EmployeeManagerTests
+{
+    [TestClass]
+    public class EmployeeDetailViewModelTests
+    {
+
+        [TestMethod]
+        public void EditEmployee()
+        {
+            EmployeesViewModel employeesViewModel = new EmployeesViewModel(null);
+
+            employeesViewModel.SearchEmployees("", 1);
+            System.Threading.Thread.Sleep(2000);
+
+            Random random = new Random();
+            int random_idx = random.Next(0, employeesViewModel.Employees.Count - 1);
+
+            Employee oldEmployee = employeesViewModel.Employees[random_idx];
+
+
+            EmployeeDetailViewModel employeeDetailViewModel = new EmployeeDetailViewModel(null, oldEmployee, true, false);
+
+            oldEmployee.Name = oldEmployee.Name + "z";
+
+            employeeDetailViewModel.SaveEmployee(oldEmployee);
+            System.Threading.Thread.Sleep(2000);
+
+            employeesViewModel.SearchEmployees("", 1);
+            System.Threading.Thread.Sleep(2000);
+
+            Employee editedEmployee = employeesViewModel.Employees[random_idx];
+
+            Assert.AreEqual(oldEmployee.Name, editedEmployee.Name);
+
+        }
+
+       
+    }
+}
